6a4aac4a490b52d5113eb9d0a479009ba8a29acb
Daniel Rosa da Silveira <danielrosasilveira@gmail.com>
2019-12-02 10:55:10 -0300

    Implementação da Classe GeralDAO.cs

--- GestaoVendas/DAL/GeralDAO.cs
+++ GestaoVendas/DAL/GeralDAO.cs
@@ -1,11 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
 
 namespace GestaoVendas.DAL
 {
     public class GeralDAO
     {
+        #region retornoDataTable
+        //Espera um parâmetro do tipo Command
+        //contendo um comando SQL do tipo SELECT
+        public DataTable retornoDataTable(MySqlCommand cmd)
+        {
+            try
+            {
+                DataTable data = new DataTable();
+                cmd.Connection = Conexao.Connection;
+                Conexao.Conectar();
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(data);
+                return data;
+            }
+            catch (Exception ex)
+            {
+                string erro = ex.Message;
+                return null;
+            }
+            finally
+            {
+                Conexao.Desconectar();
+            }
+        }
+        #endregion
+
+        #region executarSQL
+        //Espera um parâmetro do tipo string
+        //contendo um comando SQL do tipo INSERT, UPDATE, DELETE
+        public void executarSQL(MySqlCommand cmd)
+        {
+            try
+            {
+                cmd.Connection = Conexao.Connection;
+                Conexao.Conectar();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                string erro = ex.Message;
+            }
+            finally
+            {
+                Conexao.Desconectar();
+            }
+        }
+        #endregion
     }
 }
