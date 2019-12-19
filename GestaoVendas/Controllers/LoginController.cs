a2a1969ae9bddea9799d8d313c1d46dc1808e5a9
Daniel Rosa da Silveia <danielrosasilveira@gmail.com>
2019-12-19 17:43:03 -0300

    Mensagem de usuário ou senha inválido implementado

--- GestaoVendas/Controllers/LoginController.cs
+++ GestaoVendas/Controllers/LoginController.cs
@@ -12,8 +12,12 @@ namespace GestaoVendas.Controllers
     public class LoginController : Controller
     {
         #region Index
-        public IActionResult Index()
+        public IActionResult Index(string? erro)
         {
+            if (erro != null)
+            {
+                TempData["error"] = erro;
+            }
             return View();
         }
         #endregion
@@ -25,26 +29,25 @@ namespace GestaoVendas.Controllers
         {
             LoginDAO obj = new LoginDAO();
             List<VendedorModel> result = obj.validarLogin(login);
-
-            foreach (var item in result)
+            if (result.Count > 0)
             {
-                if (BCrypt.Net.BCrypt.Verify(login.Senha, item.Senha))
+                foreach (var item in result)
                 {
-                    HttpContext.Session.SetString("Idvendedor", item.Idvendedor.ToString());
-                    HttpContext.Session.SetString("Nome", item.Nome);
-                    HttpContext.Session.SetString("Sexo", item.Sexo);
-                    HttpContext.Session.SetString("Email", item.Email);
-                    HttpContext.Session.SetString("Nivel", item.Nivel);
-                    HttpContext.Session.SetString("Foto", (item.Foto != null ? 
-                        Convert.ToBase64String(item.Foto) : string.Empty));
-                    return RedirectToAction("Index", "Vendedor");
-                }
-                else
-                {
-                    TempData["error"] = "Login ou senha inválido";
+                    if (BCrypt.Net.BCrypt.Verify(login.Senha, item.Senha))
+                    {
+                        HttpContext.Session.SetString("Idvendedor", item.Idvendedor.ToString());
+                        HttpContext.Session.SetString("Nome", item.Nome);
+                        HttpContext.Session.SetString("Sexo", item.Sexo);
+                        HttpContext.Session.SetString("Email", item.Email);
+                        HttpContext.Session.SetString("Nivel", item.Nivel);
+                        HttpContext.Session.SetString("Foto", (item.Foto != null ?
+                            Convert.ToBase64String(item.Foto) : string.Empty));
+                        return RedirectToAction("Index", "Vendedor");
+                    }
                 }
             }
-            return View();
+            string erro = "Usuário ou Senha Inválido";
+            return RedirectToAction(nameof(Index), new { @erro = erro });
         }
         #endregion
     }
