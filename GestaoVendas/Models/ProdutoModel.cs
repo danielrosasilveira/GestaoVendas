e42788135e2a46d6377a963b37fcf180fe66aea4
Daniel Rosa da Silveia <danielrosasilveira@gmail.com>
2019-12-26 12:20:56 -0300

    CRUD Cliente

---
+++ GestaoVendas/Models/ProdutoModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestaoVendas.Models
+{
+    public class ProdutoModel
+    {
+        public string Idproduto { get; set; }
+
+        [Required(ErrorMessage = "Informe o Nome do Produto")]
+        public string Nome { get; set; }
+        
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Informe a Preço do Produto")]
+        public decimal? Preco_unitario { get; set; }
+
+        [Required(ErrorMessage = "Informe a Quantidade")]
+        public int Quantidade { get; set; }
+
+        public byte[] Foto { get; set; }
+
+        public string ContentType { get; set; }
+    }
+}
